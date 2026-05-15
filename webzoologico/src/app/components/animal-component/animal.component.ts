c5b2e4480ab0744f0d3f5535d417461f5c0f49bc
Jose Bernal <josef.bernalc@konradlorenz.edu.co>
2026-05-14 20:44:19 -0500

    finalización presentacion put pero no cargan los animales

--- webzoologico/src/app/components/animal-component/animal.component.ts
+++ webzoologico/src/app/components/animal-component/animal.component.ts
@@ -4,17 +4,20 @@ import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-animal-component',
   standalone: true,
-  imports: [ReactiveFormsModule], //IMPORTACION SUGERIDA IA
+  imports: [ReactiveFormsModule, CommonModule], //IMPORTACION SUGERIDA IA
   templateUrl: './animal-component.html',
   styleUrl: './animal-component.css', //corchetes sugeridos IA
 })
 export class AnimalComponent {
   animalList:any = [];
   animalForm: FormGroup | any;
+  idAnimal: any;
+  editableAnimal: boolean = false;
 
     constructor(private animalService: AnimalService,
       private formBuilder: FormBuilder,
@@ -54,5 +57,32 @@ export class AnimalComponent {
       this.getAllAnimals();
     }
 
-  
+     updateAnimalEntry() {
+    //Removiendo valores vacios del formulario de actualización
+    for (let key in this.animalForm.value) {
+      if (this.animalForm.value[key] === '') {
+        this.animalForm.removeControl(key);
+      }
+    }
+    this.animalService.updateAnimal(this.idAnimal, this.animalForm.value).subscribe(
+      () => {
+        //Enviando mensaje de confirmación
+        this.newMessage("Animal editado");
+      }
+    );
+  }
+  toggleEditAnimal(id: any) {
+    this.idAnimal = id;
+    console.log(this.idAnimal)
+    this.animalService.getOneAnimal(id).subscribe(
+      data => {
+        this.animalForm.setValue({
+          nombre: data.nombre,
+          edad: data.edad,
+          tipo: data.tipo,          
+        });
+      }
+    );
+    this.editableAnimal = !this.editableAnimal;
+  }
 }
